c46283047687dcb661d4ca14b72048920054cb58
itsshawondevs <shawonuser24@gmail.com>
2023-12-27 00:25:24 +0600

    project design all complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ tailwind.config = {
           black: '#161722',
           navbg: '#0D0D0D',
           lightgray: '#FFF5EB',
+          lightOrange: '#FFF5EB',
         },
         fontFamily: {
             'roboto': ["'Roboto Condensed', sans-serif;"],
@@ -22,6 +23,7 @@ tailwind.config = {
             'benefit-img4': "url('./images/benefit-img4.png')",
             'overlay': "linear-gradient(0deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%)",
             'text': "linear-gradient(110deg, #A54E2B -53.83%, #DC9853 62.61%)",
+            'aroundBg': "url(./images/aroundbg.jpg)"
         },
       }
     }
